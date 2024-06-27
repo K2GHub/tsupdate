6c7b5a48e3bfef2c7ecccd98245eae8a3607f278
K2GHub <98541922+K2GHub@users.noreply.github.com>
2024-06-27 13:04:09 -0400

    Update settings.py

--- thought_sync_django-main/thought_sync/settings.py
+++ thought_sync_django-main/thought_sync/settings.py
@@ -87,7 +87,6 @@ WSGI_APPLICATION = "thought_sync.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
